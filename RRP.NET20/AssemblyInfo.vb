fe76a5cd3db60c0f72a4840ec84a2e93aa4f6e55
8legd <git@8legd.com>
2015-06-02 23:27:41 +0100

    initial release

---
+++ RRP.NET20/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RRP.NET20")> 
+<Assembly: AssemblyDescription("RRP Client for .NET 2.0")> 
+<Assembly: AssemblyCompany("}}8{{ //8le.gd")> 
+<Assembly: AssemblyProduct("RRP.NET20")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4fb9b4a9-56f1-4999-b467-ae47689d16c2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
